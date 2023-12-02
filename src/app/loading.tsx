469bb33881f1ce49895139a74c1f8899936d9b86
Leke Abiodun <124153246+a-short-dev@users.noreply.github.com>
2023-12-02 13:01:48 +0100

    fix: remove all transactions-three

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className='flex w-full h-full items-center justify-center'>
+      <h3 className='text-2xl animate-pulse'>Loading</h3>
+    </div>
+  );
+}
