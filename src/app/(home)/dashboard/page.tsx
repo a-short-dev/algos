36d06b9091ca40dd1eef28d3e422744c811c9cf7
Leke Abiodun <124153246+a-short-dev@users.noreply.github.com>
2023-12-02 22:48:32 +0100

    fix: remove all transactions-14

--- src/app/(home)/dashboard/page.tsx
+++ src/app/(home)/dashboard/page.tsx
@@ -1,3 +1,6 @@
+
+
 export default async function Dashboard() {
+
   return <div>Hello</div>;
 }
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default async function name({
+const getUserDetails = async () => {};
+export default async function HomeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  getUserDetails()
   return <section>{children}</section>;
 }
